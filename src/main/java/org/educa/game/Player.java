545eb5cbcdefa6512c61b47a250f347a3797d905
Javilmar <errinostrich@gmail.com>
2024-01-25 02:55:02 +0100

    empezamos2

--- src/main/java/org/educa/game/Player.java
+++ src/main/java/org/educa/game/Player.java
@@ -1,7 +1,15 @@
 package org.educa.game;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Player extends Thread {
     private String gameType;
+    private final String HOST = "localhost";
+    private final int PORT = 5555;
+    private Socket socket;
+
     public Player(String name, String gameType) {
         super.setName(name);
         this.gameType = gameType;
@@ -11,6 +19,14 @@ public class Player extends Thread {
     public void run() {
         System.out.println("Start player");
 
-        
+        try {
+            socket = new Socket(HOST, 5555);
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            writer.println(this.getName());
+            System.out.println("Conexion realizada con exito");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
