397d32e33c8300075e4fe63a895dfb149f30b6fa
AudeTercero <92akumanomi@gmail.com>
2024-01-25 13:27:16 +0100

    d

--- src/main/java/org/educa/game/Player.java
+++ src/main/java/org/educa/game/Player.java
@@ -6,8 +6,8 @@ import java.net.Socket;
 
 public class Player extends Thread {
     private String gameType;
-    private final String HOST = "localhost";
-    private final int PORT = 5555;
+    private static final String HOST = "localhost";
+    private static final int PORT = 5555;
     private Socket socket;
 
     public Player(String name, String gameType) {
@@ -20,7 +20,7 @@ public class Player extends Thread {
         System.out.println("Start player");
 
         try {
-            socket = new Socket(HOST, 5555);
+            socket = new Socket(HOST, PORT);
             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
             writer.println(this.getName());
             System.out.println("Conexion realizada con exito");
